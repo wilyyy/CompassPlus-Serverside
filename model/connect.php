3a7c32cf086dce5f06ddf11529fbf0c8a4fb5bb5
William Laurel Alvarez <aguilazorz@hotmail.com>
2021-11-29 19:58:14 -0800

    Update connect.php

--- model/connect.php
+++ model/connect.php
@@ -1,7 +1,7 @@
 <?php
 //connect to the spreadsheet mdiatest
 try {
-  $db = new PDO("mysql:dbname=compassplus;host=143.198.158.66:3306", "compassplus", "kbx73f4kskpUvBrT");
+  $db = new PDO("mysql:dbname=compassplus;host=143.198.158.66:3306", "*********", "********");
 } catch (Exception $e){
   echo $e->getMessage();
 }
